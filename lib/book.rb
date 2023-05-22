e53b546f892ff51381691aa9797cba3c15f6102e
Victor <victor.ireri@student.moringaschool.com>
2023-05-22 21:55:43 +0300

    graded labs

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
-
+class Book
+    attr_accessor :title, :author, :page_count
+  
+    def initialize(title, author, page_count)
+      @title = title
+      @author = author
+      @page_count = page_count
+    end
+  
+    def display_info
+      puts "Title: #{@title}"
+      puts "Author: #{@author}"
+      puts "Page Count: #{@page_count}"
+    end
+  end
+  
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size
+  
+    def initialize(brand, color, size)
+      @brand = brand
+      @color = color
+      @size = size
+    end
+  
+    def description
+      "This shoe is a #{@brand} #{@color} shoe in size #{@size}."
+    end
+  end
+  
